b9f7a8739a5080255529c44b5ab6e69fb11006ac
Jericho <benitez.renzjericho0929@proton.me>
2025-10-09 15:58:05 +0800

    Extract sub-class foodFormula to own class file. Add measurement for per-meal feature.

--- app/src/main/java/com/example/petcareexpress/MainActivity.java
+++ app/src/main/java/com/example/petcareexpress/MainActivity.java
@@ -18,6 +18,7 @@ import com.example.petcareexpress.objects.FoodFormula;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     EditText inputPetWeightText, inputMealAmount;
+<<<<<<< HEAD
     TextView petWeightText, petFoodBrandText, servingWeightText, mealWeightText;
     FoodFormula foodFormula;
     private final TextWatcher inputPetWeightWatcher = new TextWatcher() {
@@ -51,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
         servingWeightText.setText(getString(R.string.grams, returnValue));
     }
+=======
+    TextView petWeightText, petFoodBrandText, servingWeightOutput, mealWeightText, servingPerMealOutput;
+    FoodFormula foodFormula;
+    double servingWeight = 0;
+>>>>>>> b67d0a2 (Extract sub-class foodFormula to own class file. Add measurement for per-meal feature.)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Activity Initialization
@@ -63,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+<<<<<<< HEAD
         // Variables
         inputPetWeightText = findViewById(R.id.input_pet_weight);
         inputMealAmount = findViewById(R.id.input_meal_amount);
@@ -73,12 +80,85 @@ public class MainActivity extends AppCompatActivity {
         mealWeightText = findViewById(R.id.meal_per_day_amount);
 
         foodFormula = new FoodFormula();
+=======
+        // Assign variables to equivalent in UI
+        petWeightText = findViewById(R.id.pet_weight);
+        petFoodBrandText = findViewById(R.id.pet_food_brand);
+        mealWeightText = findViewById(R.id.meal_per_day_amount);
+        // User-input guide UI elements
+        inputPetWeightText = findViewById(R.id.input_pet_weight);
+        inputMealAmount = findViewById(R.id.input_meal_amount);
+        // Output-to-user UI elements
+        servingWeightOutput = findViewById(R.id.serving_weight);
+        servingPerMealOutput = findViewById(R.id.serving_per_meal);
+>>>>>>> b67d0a2 (Extract sub-class foodFormula to own class file. Add measurement for per-meal feature.)
 
-        // Set TextChange Listener
+        // Add TextChange Listeners
         inputPetWeightText.addTextChangedListener(inputPetWeightWatcher);
+<<<<<<< HEAD
 
         // Initialize default values
         petFoodBrandText.setText(foodFormula.name);
         setServingWeightText(inputPetWeightText.getText().toString());
+=======
+        inputMealAmount.addTextChangedListener(inputMealAmountWatcher);
+        // Initialize classes
+        foodFormula = new FoodFormula();
+        // Initialize default values
+        petFoodBrandText.setText(foodFormula.name);
+        setServingWeightOutput(inputPetWeightText.getText().toString());
+        setMealWeightText(inputMealAmount.getText().toString());
+    }
+    private final TextWatcher inputPetWeightWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            Log.v(TAG, "inputPetWeightWatcher stand-by.");
+        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.v(TAG, "inputPetWeightWatcher receiving input.");
+        }
+        public void afterTextChanged(Editable s) {
+            String textValue = s.toString();
+            try {
+                setServingWeightOutput(textValue);
+                setMealWeightText(inputMealAmount.getText().toString());
+
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "NumberFormatException: Input is not string.");
+            }
+        }
+    };
+    private final TextWatcher inputMealAmountWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+            String textValue = s.toString();
+            try {
+                setMealWeightText(textValue);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "NumberFormatException: Input is not string.");
+            }
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            Log.v(TAG, "inputMealAmountWatcher stand-by.");
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.v(TAG, "inputMealAmountWatcher receiving input.");
+        }
+    };
+    private void setMealWeightText(String textValue) {
+        double doubleValue = Double.parseDouble(textValue);
+        double mealWeight = servingWeight / doubleValue;
+
+        servingPerMealOutput.setText(getString(R.string.grams_per_meal, mealWeight));
+    }
+    private void setServingWeightOutput(String textValue) {
+        double doubleValue = Double.parseDouble(textValue);
+        servingWeight = foodFormula.getRecommendedServing() * doubleValue;
+
+        Log.d(TAG, "Received user-input for inputPetWeightText: " + doubleValue);
+
+        servingWeightOutput.setText(getString(R.string.grams, servingWeight));
+>>>>>>> b67d0a2 (Extract sub-class foodFormula to own class file. Add measurement for per-meal feature.)
     }
 }
